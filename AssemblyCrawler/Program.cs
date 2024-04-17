5f85fac898df7f3e19294713854e2077543e3801
Alexandr Dolenko <ad@Alexandrs-MBP.lan>
2024-04-17 13:28:52 -0700

    First commit

---
+++ AssemblyCrawler/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+
+var res = new Dictionary<string, string>();
+var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+List<Assembly> allAssemblies = new List<Assembly>();
+
+foreach (string dll in Directory.GetFiles(path, "*.dll"))
+    allAssemblies.Add(Assembly.LoadFile(dll));
+Console.WriteLine("Report");
+
+allAssemblies.ForEach(assembly =>
+{ 
+    var totalNumber = 0;
+    var types = assembly.GetTypes();
+    foreach (var tp in types)
+    {
+        var numPerClass = tp.GetMethods(BindingFlags.Public | BindingFlags.Instance).Length;
+        totalNumber += numPerClass;
+    } 
+    
+    //res.Add(assembly.FullName, totalNumber.ToString());
+    Console.WriteLine($"{assembly.FullName}: {totalNumber}");
+});
